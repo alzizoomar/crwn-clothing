f276893b8bc4675a6993a10b4883a0b5e99f2f33
alzizoomar <alzizoomar@gmail.com>
2021-05-14 23:22:55 +0200

    use styled-component package to style my project

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_51IqimgCNgQntXWRRstbT3Bqm2I2wWUrXINpJb591rLMruOEJgNhIxHINqNZFdfEszxnthiFG2X4oOywenkmXuPNa00I7NkCJga';
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
   const onToken = token => {
     console.log(token);

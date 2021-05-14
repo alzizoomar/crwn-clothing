04f108f6d51a0f2a22f92cd1d0b0c6e225822a4a
alzizoomar <alzizoomar@gmail.com>
2021-05-14 15:16:47 +0200

    test auto build in heroku

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -20,7 +20,7 @@ const Header = ({ currentUser, hidden }) => (
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
-        SHOP
+        MY SHOP
       </Link>
       <Link className='option' to='/shop'>
         CONTACT
